9ba3c849168f995bae91d55522cf57e397ecbbc1
Tomasz_Ch <Tomasz_Ch@MOSCOV.pdtec-pl.lan>
2018-07-18 15:42:13 +0200

    executing in parallel

--- IncomeOutcomeAggregator/IncomeOutcomeAggregator/src/main/java/tchpl/incomeoutcomeaggregator/Runner.java
+++ IncomeOutcomeAggregator/IncomeOutcomeAggregator/src/main/java/tchpl/incomeoutcomeaggregator/Runner.java
@@ -1,10 +1,18 @@
 package tchpl.incomeoutcomeaggregator;
 
+import com.sun.org.apache.xerces.internal.impl.dv.xs.DateTimeDV;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  *
@@ -13,6 +21,20 @@ import java.util.logging.Logger;
 public class Runner {
 
     public static void main(String[] args) {
+        Runner runner = new Runner();
+        try {
+            runner.run(args);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Runner.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ExecutionException ex) {
+            Logger.getLogger(Runner.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void run(String[] args) throws InterruptedException, ExecutionException {
+        if (args == null || args.length == 0 || args[0] == null) {
+            throw new IllegalStateException("!!!");
+        }
         DataReaderFactory factory = new DataReaderFactory();
         List<DataSingleLineItem> items = new ArrayList<>();
         List<String> foodDescriptions = new ArrayList<>();
@@ -32,23 +54,54 @@ public class Runner {
         acceptors.add(acceptor);
         acceptors.add(acceptor2);
         acceptors.add(acceptorApteka);
+        runNormal(acceptors, args[0]);
+        runParallel(acceptors, args[0]);
+    }
 
+    private void runNormal(List<Acceptor> acceptors, String path) {
+        Date start = new Date();
         for (Acceptor a : acceptors) {
-            try {
-                DataReader reader = factory.getStandardReadrer(args[0]);
-                reader.getData().forEach(s -> items.add(new DataSingleLineItem(s)));
-                BigDecimal total = BigDecimal.ZERO;
-                for (DataSingleLineItem single : items) {
-                    if (a.accept(single)) {
-                        BigDecimal b = new BigDecimal(Double.valueOf(single.getValue(Type.AMOUNT)));
-                        total = total.add(b);
-                    }
-                }
-                System.out.println(a.getName() + ":" + total);
-            } catch (Exception ex) {
-                Logger.getLogger(Runner.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            System.out.println(aggregate(path, a));
         }
+        Date end = new Date();
+        System.out.println(String.format("Normal:%s", end.getTime() - start.getTime()));
+    }
 
+    private void runParallel(List<Acceptor> acceptors, String path) throws InterruptedException, ExecutionException {
+        Date start = new Date();
+        List<CompletableFuture> tasks = new ArrayList<>();
+        for (Acceptor a : acceptors) {
+            Supplier<BigDecimal> sup = () -> aggregate(path, a);
+            tasks.add(CompletableFuture.supplyAsync(sup));
+        }
+        CompletableFuture<Void> combinedFuture
+                = CompletableFuture.allOf(tasks.toArray(new CompletableFuture[tasks.size()]));
+        combinedFuture.get();
+        for (CompletableFuture t : tasks) {
+            System.out.println(t.get());
+        }
+        Date end = new Date();
+        System.out.println(String.format("Parallel:%s", end.getTime() - start.getTime()));
+    }
+
+    BigDecimal aggregate(String dataSourcePath, Acceptor acceptor) {
+        DataReaderFactory factory = new DataReaderFactory();
+        List<DataSingleLineItem> items = new ArrayList<>();
+        BigDecimal total = BigDecimal.ZERO;
+        try {
+            DataReader reader = factory.getStandardReadrer(dataSourcePath);
+            reader.getData().forEach(s -> items.add(new DataSingleLineItem(s)));
+
+            for (DataSingleLineItem single : items) {
+                if (acceptor.accept(single)) {
+                    BigDecimal b = new BigDecimal(Double.valueOf(single.getValue(Type.AMOUNT)));
+                    total = total.add(b);
+                }
+            }
+//            System.out.println(acceptor.getName() + ":" + total);
+        } catch (Exception ex) {
+            Logger.getLogger(Runner.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return total;
     }
 }
