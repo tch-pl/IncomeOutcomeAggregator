7659a4133988d57d088aedc04bcd959cf10dcb1b
Tomasz_Ch <Tomasz_Ch@MOSCOV.pdtec-pl.lan>
2018-09-24 13:13:35 +0200

    executing in parallel

---
+++ IncomeOutcomeAggregator/IncomeOutcomeAggregator/src/test/java/tchpl/incomeoutcomeaggregator/AcceptorTest.java
@@ -0,0 +1,33 @@
+package tchpl.incomeoutcomeaggregator;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Tomasz_Ch
+ */
+public class AcceptorTest {
+    
+    public AcceptorTest() {
+    }
+
+    @Test
+    public void testAccept() {     
+        String line = "\"2018-07-02\",\"2018-06-30\",\"P³atnoœæ kart¹\",\"-15.60\",\"PLN\",\"+53207.83\",\"Tytu³: 000498849 74230788182058952292816\",\"Lokalizacja: Kraj: POLSKA Miasto: RUMIA Adres: GOTUS SP. Z O.O.\",\"Data i czas operacji: 2018-06-30\",\"Oryginalna kwota operacji: 15,60 PLN\",\"Numer karty: 42512509*****539\",\"\"";
+        DataSingleLineItem item = new DataSingleLineItem(line);
+        List<String> matching = new ArrayList<>();
+        matching.add("GOTUS");
+                
+        Acceptor instance = new Acceptor(Type.DESCRIPTION, matching, line);
+        boolean expResult = true;
+        boolean result = instance.accept(item);
+        assertEquals(expResult, result);
+        
+    }
+
+    
+    
+}
